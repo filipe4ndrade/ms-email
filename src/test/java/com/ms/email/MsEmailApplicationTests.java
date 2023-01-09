c251aacbccd53956ba2b4554355a25b4f6d53efa
Filipe <filipeandrade000@gmail.com>
2023-01-09 01:44:31 -0300

    Primeiro Commit

---
+++ src/test/java/com/ms/email/MsEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
